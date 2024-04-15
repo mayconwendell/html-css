68ceb2ca2281f6a44b78f9713bd99030d61cab51
MayconWendell <164102300+MayconWendell@users.noreply.github.com>
2024-04-15 11:39:30 -0300

    Initial commit

---
+++ testes/Teste002/script.js
@@ -0,0 +1,5 @@
+// Aqui você pode adicionar interações adicionais, como animações, validação de formulários, etc.
+// Por exemplo:
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JavaScript aqui
+});
