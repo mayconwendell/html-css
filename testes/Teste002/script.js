19d13f39564f77dbe3c952c312ef33d52e63010c
MayconWendell <164102300+MayconWendell@users.noreply.github.com>
2024-04-19 10:15:46 -0300

    new

--- testes/Teste002/script.js
+++
@@ -1,5 +0,0 @@
-// Aqui você pode adicionar interações adicionais, como animações, validação de formulários, etc.
-// Por exemplo:
-document.addEventListener('DOMContentLoaded', function() {
-    // Código JavaScript aqui
-});
